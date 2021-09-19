28fe2a19c3b68208fbaebe3f322db757b5ca6a1c
Willian Camacho <61927270+Will2020-cpu@users.noreply.github.com>
2021-09-19 03:36:48 -0500

    Agregando front end con nextjs
    Angular JS no es muy bueno con los svg :-1:

--- next-client/pages/index.js
+++ next-client/pages/index.js
@@ -1,82 +1,52 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import React,{ useState } from 'react';
 
-export default function Index() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+export default function Home() {
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+    /*Manejando el formulario*/
+    const signinForm = async event =>{
+        event.preventDefault();
+        const res = await fetch(
+            `https://localhost:8080/login?username=${event.target.name.value}&password=${event.target.password.value}"`
+        )
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+    }
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+    return(
+        <>
+            <div className="flex items-center justify-center relative">
+                <span className="absolute top-12 py-2 px-5 text-sm bg-red-500 rounded-sm text-white">
+                    Inicio de sesion
+                </span>
+            
+                <div className="w-max mx-auto h-screen flex items-center justify-center ">
+                    {/* Header form  */}
+                    <div className="border shadow-lg rounded-lg">
+                        <div className="border-b py-2">
+                            <div className="px-20">
+                                <h4 className="text-xl text-center">Iniciar sesion</h4>
+                            </div>
+                        </div>
+                        {/* Body form */}
+                        <div className="flex items-center justify-center px-10 py-8">
+                            <form  className="space-y-4">
+                                <div className="flex flex-col space-y-1">
+                                    <label htmlFor="inputUsername" className="text-sm">Username</label>
+                                    <input type="username" autoComplete="username" required id="inputUsername" placeholder="spiderman" className="border p-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-transparent" />
+                                </div>
+                                <div className="flex flex-col space-y-1">
+                                    <label htmlFor="inputPassword">Password</label>
+                                    <input type="password" id="inputPassword" placeholder="******"  className="border p-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-transparent"/>
+                                </div>
+                                <div className="w-full">
+                                    <button type="submit" className="w-full p-2 text-white rounded-sm bg-black">Ingresar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
